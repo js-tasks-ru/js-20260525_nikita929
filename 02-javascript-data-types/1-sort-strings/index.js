1e4c4e3ef1758e9e8e20b099327dfa1260288961
NolikDM <mr.delete13@gmail.com>
2026-06-10 23:41:55 +0700

    added sortStrings task

--- 02-javascript-data-types/1-sort-strings/index.js
+++ 02-javascript-data-types/1-sort-strings/index.js
@@ -4,6 +4,10 @@
  * @param {string} [param="asc"] param - the sorting type "asc" or "desc"
  * @returns {string[]}
  */
-export function sortStrings(arr, param = 'asc') {
+export function sortStrings(arr, param = "asc") {
+  const collator = new Intl.Collator(["ru", "en"], { caseFirst: "upper" });
 
+  return [...arr].sort(
+    param === "desc" ? (a, b) => collator.compare(b, a) : collator.compare,
+  );
 }
